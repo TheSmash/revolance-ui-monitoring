14f2ed5a0446105508c3cf18898b550258ec3870
wsmash <bethesmash@gmail.com>
2014-01-19 14:10:26 +0100

    moving smoke tests from itests to distrib module

--- distrib/src/test/java/com/smash/revolance/ui/tests/smoke/ServerLifecycleTest.java
+++ distrib/src/test/java/com/smash/revolance/ui/tests/smoke/ServerLifecycleTest.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ServerLifecycleTest extends TestConstants
